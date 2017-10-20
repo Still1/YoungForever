e132fe1c0aff1e80614cec693cb713a666f23b01
Still1 <oliverstill1@gmail.com>
2017-10-20 12:26:12 +0800

    Developing first query from grid.
    Signed-off-by: Still1 <oliverstill1@gmail.com>

--- young-forever-book/src/main/resources/template/book/easyUI/book.js
+++ young-forever-book/src/main/resources/template/book/easyUI/book.js
@@ -1,7 +1,7 @@
 (function () {
 	$(document).ready(function() {
 		$("#bookGrid").datagrid({
-			url : 'data/book/bookGrid?cond=[{name\=id,value\=1,op\=eq},{name\=name,value\=aa,op\=eq}]',
+			url : 'data/book/bookGrid?',
 			idField : 'id',
 			method : 'get',
 			
@@ -9,11 +9,35 @@
 			pagination : true,
 			rownumbers : true,
 			
+			frozenColumns : [[
+				{field : 'chinese_name', title : '书名', sortable : true},
+				{field : 'name', title : '原书名', sortable : true},
+				{field : 'author_name', title : '作者'},
+				{field : 'buy_state', title : '购买状态', formatter : function(value) {
+					if(value) {
+						return "已购买";
+					} else {
+						return "未购买";
+					}
+				}},
+				{field : 'douban_value', title : '豆瓣评分', sortable : true},
+				{field : 'douban_count', title : '评价人数', sortable : true},
+			]],
 			columns : [[
-				{field : 'id', hidden : true},
-				{field : 'chinese_name', title : '书名', width : '500'},
-				{field : 'name', title : '原书名', width : '500'},
-				{field : 'author_name', title : '作者', width : '500'}
+				{field : 'subtitle', title : '副标题'},
+				{field : 'translator', title : '译者'},
+				{field : 'publisher', title : '出版社'},
+				{field : 'publication_date', title : '出版时间'},
+				{field : 'page', title : '页数'},
+				{field : 'price', title : '定价'},
+				{field : 'book_binding', title : '装帧', formatter : function(value) {
+					if(value == 1) {
+						return "平装";
+					} else if(value == 2) {
+						return "精装";
+					}
+				}},
+				{field : 'isbn', title : 'ISBN'}
 			]],
 			toolbar : [{
 				iconCls : 'icon-add',

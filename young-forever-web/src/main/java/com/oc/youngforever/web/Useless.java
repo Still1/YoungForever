dacd3c801e05eba9eeb97c78148fef17fe7f46cf
Still1 <Still1@Still1>
2018-08-05 10:02:26 +0800

    Add "useless" to make the folder adding to git.

---
+++ young-forever-web/src/main/java/com/oc/youngforever/web/Useless.java
@@ -0,0 +1,8 @@
+package com.oc.youngforever.web;
+/**
+ * This file is used for taking the position, in order to add this folder to git.
+ * When you add other files to this folder, you can delete this.
+ */
+public class Useless {
+
+}
